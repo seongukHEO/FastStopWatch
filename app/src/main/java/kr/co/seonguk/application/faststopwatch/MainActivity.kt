bfba87bfe785d5f8b31f349d5e152a1f49bb94e4
seongukHeo <heoseonguk3@gmail.com>
2024-05-04 11:42:56 +0900

    feat : 스톱워치 start pause 기능 구현

--- app/src/main/java/kr/co/seonguk/application/faststopwatch/MainActivity.kt
+++ app/src/main/java/kr/co/seonguk/application/faststopwatch/MainActivity.kt
@@ -6,10 +6,18 @@ import android.os.Bundle
 import androidx.core.view.isVisible
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kr.co.seonguk.application.faststopwatch.databinding.ActivityMainBinding
+import kr.co.seonguk.application.faststopwatch.databinding.DialogCountdownBinding
+import java.util.Timer
+import kotlin.concurrent.timer
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private var countDownSecond = 10
+    private var currentDeciSecond = 0
+    private var timer : Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun settingEvent(){
+    private fun settingEvent() {
         binding.apply {
             buttonStart.setOnClickListener {
                 putStart()
@@ -48,37 +56,71 @@ class MainActivity : AppCompatActivity() {
                 buttonLab.isVisible = false
                 buttonPause.isVisible = false
             }
+            textviewCountDown.setOnClickListener {
+                showCountDownDialog()
+            }
+
         }
     }
 
 
     //시작 버튼을 눌렀을 때
-    private fun putStart(){
+    private fun putStart() {
+        timer = timer(initialDelay = 0, period = 100) {
+            currentDeciSecond += 1
 
+            val minutes = currentDeciSecond.div(10) / 60
+            val second = currentDeciSecond.div(10) % 60
+            val deciSecond = currentDeciSecond % 10
+
+            runOnUiThread {
+                binding.textTimeView.text = String.format("%02d:%02d", minutes, second)
+                binding.textviewTic.text = deciSecond.toString()
+            }
+        }
     }
 
     //종료 버튼을 눌렀을 때
-    private fun putStop(){
+    private fun putStop() {
 
     }
 
     //일시정지
-    private fun putPause(){
-
+    private fun putPause() {
+        timer?.cancel()
+        timer = null
     }
 
     //기록
-    private fun putLab(){
+    private fun putLab() {
 
     }
 
 
+    private fun showCountDownDialog() {
+        AlertDialog.Builder(this).apply {
+            setTitle("CountDown 시간 설정")
+            val dialogBinding = DialogCountdownBinding.inflate(layoutInflater)
+            setView(dialogBinding.root)
+            with(dialogBinding.countDownSecondPicker) {
+                maxValue = 20
+                minValue = 0
+                value = countDownSecond
+            }
+            setPositiveButton("확인") { _, _ ->
+                countDownSecond = dialogBinding.countDownSecondPicker.value
+                binding.textviewCountDown.text = String.format("%02d", countDownSecond)
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
     //다이얼로그
-    private fun showAlertDialog(){
+    private fun showAlertDialog() {
         AlertDialog.Builder(this).apply {
             setTitle("스톱워치 초기화")
             setMessage("스톱워치를 초기화 하시겠습니까?")
-            setPositiveButton("확인"){_, _ ->
+            setPositiveButton("확인") { _, _ ->
                 putStop()
             }
             setNegativeButton("취소", null)
